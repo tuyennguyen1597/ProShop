cbf94c7a8fd312053cae2d5ccf2f187078f862b3
Thi Bich Tuyen Nguyen <thibichtuyen.nguyen@student.unsw.edu.au>
2021-07-21 13:59:39 +1000

    Fetch data from database

---
+++ back-end/router/productRoute.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import Product from '../models/productModel.js'
+import asyncHandler from 'express-async-handler'
+
+const productRouter = express.Router()
+
+//@desc     Featch all products
+//@route    GET api/products
+//@access   Public
+productRouter.get('/', asyncHandler (async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+}));
+
+//@desc     Featch single product
+//@route    GET api/products/:id
+//@access   Public
+productRouter.get('/:id', asyncHandler (async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+        res.json(product)
+    } else {
+        res.status(404).json({message : 'Product Not Found'})
+    }
+    
+}))
+
+export default productRouter
--- back-end/server.js
+++ back-end/server.js
@@ -3,6 +3,7 @@ import products from './data/products.js';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js'
+import productRouter from './router/productRoute.js'
 const app = express();
 
 dotenv.config();
@@ -13,14 +14,8 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-});
+app.use('/api/products', productRouter);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id == req.params.id )
-    res.json(product)
-})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode  on port ${PORT}`.yellow.bold));
