ff3f31bdf02c8997325d41cb08d11ac392b32c01
Thi Bich Tuyen Nguyen <thibichtuyen.nguyen@student.unsw.edu.au>
2021-07-22 07:40:00 +1000

    Set custom errorHandler

--- back-end/router/productRoute.js
+++ back-end/router/productRoute.js
@@ -20,7 +20,8 @@ productRouter.get('/:id', asyncHandler (async (req, res) => {
     if (product) {
         res.json(product)
     } else {
-        res.status(404).json({message : 'Product Not Found'})
+        res.status(404)
+        throw new Error('Product not found')
     }
     
 }))
--- back-end/server.js
+++ back-end/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js'
 import productRouter from './router/productRoute.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 const app = express();
 
 dotenv.config();
@@ -16,6 +17,9 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRouter);
 
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode  on port ${PORT}`.yellow.bold));
